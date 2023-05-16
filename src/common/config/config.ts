cf0d2be801391b3f8340e158804c179567f59e52
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-05-16 11:55:15 +0100

    imprve: added alternate port number

--- src/common/config/config.ts
+++ src/common/config/config.ts
@@ -1,4 +1,4 @@
-const PORT: number = parseInt(<string>process.env.PORT);
+const PORT: number = parseInt(<string>process.env.PORT) || 3000;
 const DB_NAME: string = process.env.DB_NAME;
 const Mongoose_URL: string = process.env.DB_URL;
 const environment: string = process.env.NODE_ENV;
