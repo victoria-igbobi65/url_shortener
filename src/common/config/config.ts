f5cdff8034ce39e4f69e492f876f117f9a9e1e5c
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-05-16 22:18:20 +0100

    new: added config variables

--- src/common/config/config.ts
+++ src/common/config/config.ts
@@ -1,4 +1,5 @@
 const PORT: number = parseInt(<string>process.env.PORT) || 3000;
+const BASE_URL: string = process.env.BASE_URL;
 const DB_NAME: string = process.env.DB_NAME;
 const Mongoose_URL: string = process.env.DB_URL;
 const environment: string = process.env.NODE_ENV;
@@ -6,6 +7,7 @@ const environment: string = process.env.NODE_ENV;
 export default {
   PORT,
   DB_NAME,
+  BASE_URL,
   Mongoose_URL,
   environment,
 };
