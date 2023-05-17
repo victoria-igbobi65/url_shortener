caae92b066f2741959cb16a58af62e97bf6d3881
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-05-17 12:15:42 +0100

    new: added request info feature

--- src/url/url.service.ts
+++ src/url/url.service.ts
@@ -10,10 +10,16 @@ import {
   E_LONG_URL_EXISTS,
   E_LONG_URL_NOT_EXISTS,
 } from 'src/common/constants.text';
+import { RequestInfo } from './models/request-info.model';
+import { GetLinkDto } from './dto/get-link-info.dto';
+import { Connection } from './interface/response';
 
 @Injectable()
 export class UrlService {
-  constructor(@InjectModel(Url.name) private UrlModel: Model<Url>) {}
+  constructor(
+    @InjectModel(Url.name) private UrlModel: Model<Url>,
+    @InjectModel(RequestInfo.name) private RequestModel: Model<RequestInfo>,
+  ) {}
 
   async create(url: CreateUrlDto): Promise<{ data: Url }> {
     const existingUrl = await this.getmappingByLongURl(url.longUrl);
@@ -39,14 +45,28 @@ export class UrlService {
     return `This action returns all url`;
   }
 
-  async getLongUrl(shortId: string) {
+  async getLongUrl(shortId: string, ip: string, agent: string) {
     const data = await this.UrlModel.findOne({ shortId }).exec();
     if (!data) throw new HttpException(E_LONG_URL_NOT_EXISTS, 404);
 
-    /*Update Url count*/
-    data.count++;
-    console.log(data);
+    data.count++; /*Update Url count*/
+
+    /*Save request information*/
+    await this.RequestModel.create({
+      ip: ip,
+      agent: agent,
+      url_id: data._id,
+    });
     return await data.save();
+  }
+
+  async findone(id: GetLinkDto): Promise<Connection> {
+    const data = await this.UrlModel.findOne({ _id: id });
+    if (!data) {
+      throw new HttpException(E_LONG_URL_NOT_EXISTS, 404);
+    }
+    const urlRequestInfo = await this.RequestModel.find({ url_id: id }).exec();
+    return { data, count: urlRequestInfo.length, reqInfo: urlRequestInfo };
   }
 
   update(id: number) {
