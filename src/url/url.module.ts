3701ac0b35622c465769ebb5f7cc518a22752583
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-05-17 12:15:12 +0100

    new: registered reuest info model

--- src/url/url.module.ts
+++ src/url/url.module.ts
@@ -4,10 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UrlService } from './url.service';
 import { UrlController } from './url.controller';
 import { Url, shortUrlSchema } from './models/url.model';
+import { RequestInfo, RequestInfoschema } from './models/request-info.model';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Url.name, schema: shortUrlSchema }]),
+    MongooseModule.forFeature([
+      { name: Url.name, schema: shortUrlSchema },
+      { name: RequestInfo.name, schema: RequestInfoschema },
+    ]),
   ],
   controllers: [UrlController],
   providers: [UrlService],
