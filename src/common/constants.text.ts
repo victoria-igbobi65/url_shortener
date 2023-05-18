51f804ad5649401e89df988138bc6371e7202cb1
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-05-18 11:24:50 +0100

    improve: updated file

--- src/common/constants.text.ts
+++ src/common/constants.text.ts
@@ -3,3 +3,4 @@ export const E_TOO_MANY_REQUEST =
   'You have made too made requests to this server, try again soon!';
 export const E_LONG_URL_EXISTS = 'Long url already exists';
 export const E_LONG_URL_NOT_EXISTS = 'Url not found';
+export const E_EMAIL_EXISTS = 'User with email exists';
