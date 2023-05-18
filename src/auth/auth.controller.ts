3c821b88449bc54e3c978200456a90f6bbe3cf58
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-05-18 16:56:46 +0100

    new: added signin endpoint and authenticated logged user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() dto: CreateUserDto) {
+  signUp(@Body() dto: CreateUserDto) {
     return this.authService.register(dto);
+  }
+
+  @Post('signin')
+  @HttpCode(200)
+  signIn(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
   }
 }
