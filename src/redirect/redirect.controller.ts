a791ac54078390da679f5ecd1f6825f8086a0eaf
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-05-18 16:59:16 +0100

    new: redirect controller

---
+++ src/redirect/redirect.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Param, Res, Req, Get } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { UrlService } from 'src/url/url.service';
+
+@Controller('/redirect')
+export class RedirectController {
+  constructor(private readonly urlService: UrlService) {}
+
+  @Get(':shortCode')
+  async handleRedirect(
+    @Param('shortCode') shortCode: string,
+    @Res() res: Response,
+    @Req() req: Request,
+  ): Promise<void> {
+    const ip = req.ip,
+      agent = req.headers['user-agent'];
+
+    const url = await this.urlService.getLongUrl(shortCode, ip, agent);
+    return res.redirect(301, url.longUrl);
+  }
+}
