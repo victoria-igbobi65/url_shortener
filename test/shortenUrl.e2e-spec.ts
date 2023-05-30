b0fe0cc34c05527c3bb386bba0d7399f9653fe25
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-05-30 13:35:46 +0100

    new: e2e2 test file for shortening long url

--- test/shortenUrl.e2e-spec.ts
+++ test/shortenUrl.e2e-spec.ts
@@ -3,13 +3,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { Model } from 'mongoose';
 
+import { User } from 'src/auth/models/user.model';
 import { Url } from 'src/url/models/url.model';
-import { AuthService } from '../src/auth/auth.service';
 import { AppModule } from '../src/app.module';
+import { H_LOGINDTO, H_URLS, H_SIGNUPDTO } from './test-helper';
 
 describe('UrlController (e2e)', () => {
   let app: INestApplication;
   let urlModel: Model<Url>;
+  let authModel: Model<User>;
   let authToken: string;
 
   beforeAll(async () => {
@@ -18,44 +20,59 @@ describe('UrlController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    urlModel = moduleFixture.get<Model<Url>>('UserModel');
+    urlModel = moduleFixture.get<Model<Url>>('UrlModel');
+    authModel = moduleFixture.get<Model<User>>('UserModel');
     await app.init();
-
-    // Mock the authentication process and generate a JWT token
-    const authService = moduleFixture.get(AuthService);
-    jest
-      .spyOn(authService, 'login')
-      .mockResolvedValue({ data: null, token: 'mocked_token' }); // Mock user validation
-    const user = { id: 'mockUserId', email: 'test@example.com', name: 'test' }; // Mock user object
-    authToken = await authService.signToken(user as any); // Generate JWT token
-    console.log(authToken);
   });
 
   beforeEach(async () => {
     await urlModel.deleteMany({});
+    await authModel.deleteMany({});
+
+    await request(app.getHttpServer()).post('/auth/signup').send(H_SIGNUPDTO);
+
+    const login = await request(app.getHttpServer())
+      .post('/auth/signin')
+      .send(H_LOGINDTO);
+    authToken = login.body.token;
   });
 
   afterAll(async () => {
     await app.close();
   });
 
-  // it('should access authenticated endpoint successfully', async () => {
-  //   const response = await request(app.getHttpServer())
-  //     .post('/url')
-  //     .set('Authorization', `Bearer ${authToken}`)
-  //     .expect(HttpStatus.OK);
-
-  //   // Assert the expected response or behavior
-  //   expect(response.body).toHaveProperty('data');
-  //   // ...
-  // });
+  it('should access authenticated endpoint successfully', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/url')
+      .set('Authorization', `Bearer ${authToken}`)
+      .send(H_URLS[0])
+      .expect(HttpStatus.CREATED);
+    expect(response.body.data).toHaveProperty('longUrl');
+    expect(response.body.data).toHaveProperty('shortUrl');
+    expect(response.body.data).toHaveProperty('shortId');
+    expect(response.body.data).toHaveProperty('ownerId');
+    expect(response.body.data).toHaveProperty('_id');
+  });
 
   it("should return a 401 error if user isn't authorized", async () => {
-    const createUrlDto = { originalUrl: 'https://example.com' };
-
     await request(app.getHttpServer())
       .post('/url')
-      .send(createUrlDto)
+      .send(H_URLS[0])
       .expect(HttpStatus.UNAUTHORIZED);
+  });
+
+  it('should return a 409 error code when a URL has already been shortened', async () => {
+    await request(app.getHttpServer())
+      .post('/url')
+      .set('Authorization', `Bearer ${authToken}`)
+      .send(H_URLS[0]);
+
+    const response = await request(app.getHttpServer())
+      .post('/url')
+      .set('Authorization', `Bearer ${authToken}`)
+      .send(H_URLS[0])
+      .expect(HttpStatus.CONFLICT);
+    expect(response.body).toHaveProperty('message');
+    expect(response.body.message).toBe('Long url already exists');
   });
 });
