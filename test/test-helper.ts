570a23ec5ce3f58e9149f5ea7f1d74752df1c887
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-05-30 13:36:01 +0100

    new: test helper file

---
+++ test/test-helper.ts
@@ -0,0 +1,29 @@
+export const H_LOGINDTO = {
+  email: 'john@example.com',
+  password: 'password',
+};
+export const H_SIGNUPDTO = {
+  name: 'John Doe',
+  email: 'john@example.com',
+  password: 'password',
+};
+
+export const H_USER2 = {
+  email: 'test@example.com',
+  password: 'password',
+};
+
+export const H_USER2_SIGN = {
+  email: 'test@example.com',
+  password: 'password',
+  name: 'test',
+};
+export const H_URLS = [
+  { longUrl: 'https://example1.com' },
+  { longUrl: 'https://example2.com' },
+  { longUrl: 'https://example3.com' },
+  { longUrl: 'https://example4.com' },
+  { longUrl: 'https://example5.com' },
+  { longUrl: 'https://example6.com' },
+];
+export const H_FAKE_SHORTURL = 'http://localhost:5000/ZJ2PxPIekeS_fMteom9Mq';
