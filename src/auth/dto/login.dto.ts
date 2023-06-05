fc005a89dd7a173f0490b8ebc632b844de04caa5
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-06-05 13:24:38 +0100

    feat: swagger documentation

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,13 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDefined, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly email: string;
 
   @IsString()
   @IsDefined()
   @IsNotEmpty()
+  @ApiProperty()
   readonly password: string;
 }
