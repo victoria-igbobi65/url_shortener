d19ba23650d89b6dabfd0da5bfed5ebf13e2d6a1
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-06-05 13:23:41 +0100

    feat: swagger documentation

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,19 +1,23 @@
 import { IsDefined, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsDefined()
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly name: string;
 
   @IsEmail()
   @IsString()
   @IsDefined()
   @IsNotEmpty()
+  @ApiProperty()
   readonly email: string;
 
   @IsDefined()
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   readonly password: string;
 }
