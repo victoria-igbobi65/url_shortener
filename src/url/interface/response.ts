0340d32551216937baeea2a75a2b208014d934a7
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-06-05 13:30:22 +0100

    feat: swagger documentation

--- src/url/interface/response.ts
+++ src/url/interface/response.ts
@@ -1,10 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { RequestInfo } from '../models/request-info.model';
 import { Url } from '../models/url.model';
 
 export class Connection {
+  @ApiProperty()
   data: Url;
 
+  @ApiProperty()
   count: number;
 
+  @ApiProperty()
   reqInfo: RequestInfo[];
+}
+
+export class CreateUrlInterface {
+  @ApiProperty()
+  data: Url;
+}
+
+export class QrUrlInterface {
+  @ApiProperty()
+  data: string;
+}
+
+export class UrlAllInterface {
+  @ApiProperty()
+  count: number;
+
+  @ApiProperty()
+  data: Url[];
 }
