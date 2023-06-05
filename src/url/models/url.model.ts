bd1631b704a9d3d72b90fdec471940384fa0376d
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-06-05 13:31:40 +0100

    feat: swagger documentation

--- src/url/models/url.model.ts
+++ src/url/models/url.model.ts
@@ -1,23 +1,30 @@
 import { Document, Schema as MongooseSchema } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Schema()
 export class Url extends Document {
+  @ApiProperty()
   @Prop({ required: true })
   longUrl: string;
 
+  @ApiProperty()
   @Prop({ required: true })
   shortUrl: string;
 
+  @ApiProperty()
   @Prop({ required: true })
   shortId: string;
 
+  @ApiProperty()
   @Prop({ type: MongooseSchema.Types.ObjectId, required: true })
   ownerId: MongooseSchema.Types.ObjectId;
 
+  @ApiProperty()
   @Prop({ required: true, default: Date.now })
   createdAt: Date;
 
+  @ApiProperty()
   @Prop({ default: 0 })
   count: number;
 }
