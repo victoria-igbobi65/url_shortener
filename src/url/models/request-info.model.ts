3d360c04df251341258f6ec75e3cd8f844411605
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-06-05 13:31:00 +0100

    feat: sawgger documentation

--- src/url/models/request-info.model.ts
+++ src/url/models/request-info.model.ts
@@ -1,17 +1,22 @@
 import { Document, Schema as MongooseSchema } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Schema()
 export class RequestInfo extends Document {
+  @ApiProperty()
   @Prop({ required: true })
   ip: string;
 
+  @ApiProperty()
   @Prop({ required: true })
   agent: string;
 
+  @ApiProperty()
   @Prop({ type: MongooseSchema.Types.ObjectId, required: true })
   url_id: MongooseSchema.Types.ObjectId;
 
+  @ApiProperty()
   @Prop({ required: true, default: Date.now })
   createdAt: Date;
 }
