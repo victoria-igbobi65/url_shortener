38af631bb988de2db0544ee07b957f219e67ccd4
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-06-05 13:27:32 +0100

    feat: swagger documentation

--- src/url/url.controller.ts
+++ src/url/url.controller.ts
@@ -10,24 +10,49 @@ import {
 import { AuthGuard } from '@nestjs/passport';
 import { Response } from 'express';
 import * as qrcode from 'qrcode';
+import {
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 import { UrlService } from './url.service';
 import { CreateUrlDto } from './dto/create-url.dto';
 import { GetLinkDto } from './dto/get-link-info.dto';
 import { CurrentUser } from '../auth/decorator/current-user.decorator';
 import { User } from '../auth/models/user.model';
+import {
+  E_LONG_URL_EXISTS,
+  E_LONG_URL_NOT_EXISTS,
+} from 'src/common/constants.text';
+import {
+  CreateUrlInterface,
+  QrUrlInterface,
+  UrlAllInterface,
+  Connection,
+} from './interface/response';
 
+@ApiTags('url')
 @UseGuards(AuthGuard('jwt'))
 @Controller('url')
 export class UrlController {
   constructor(private readonly urlService: UrlService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: CreateUrlInterface })
+  @ApiConflictResponse({ description: E_LONG_URL_EXISTS })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   create(@Body() createUrlDto: CreateUrlDto, @CurrentUser() user: User) {
     return this.urlService.create(createUrlDto, user._id);
   }
 
   @Post('qrcode')
+  @ApiOkResponse({ type: QrUrlInterface })
+  @ApiConflictResponse({ description: E_LONG_URL_EXISTS })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   async createQr(
     @Body() dto: CreateUrlDto,
     @CurrentUser() user: User,
@@ -39,11 +64,16 @@ export class UrlController {
   }
 
   @Get()
+  @ApiOkResponse({ type: UrlAllInterface })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   findAll(@CurrentUser() user: User) {
     return this.urlService.findAll(user._id);
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: Connection })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: E_LONG_URL_NOT_EXISTS })
   async GetUrlInfo(@Param() { id }: GetLinkDto, @CurrentUser() user: User) {
     return this.urlService.findone(id, user._id);
   }
